c8833499b7a30b97aa2f0a9c8457d6273c6647a2
AbdurRahman11072 <rjrahman014@gmail.com>
2025-11-24 09:01:53 +0600

    show all the output

--- fs_append.js
+++ fs_append.js
@@ -1,15 +1,15 @@
 const { error } = require("console");
 const fs = require("fs");
-// //* fs.writeFileSync to create file
-// fs.writeFileSync("./output/app.log", "Application started");
+//* fs.writeFileSync to create file
+fs.writeFileSync("./output/app.log", "Application started");
 
-// //* fs.appendFileSync to write something on the file
-// const logEntry1 = `\n ${new Date().toISOString()}`;
-// fs.appendFileSync("./output/app.log", logEntry1);
-// const logEntry2 = `\n ${new Date().toISOString()}`;
-// fs.appendFileSync("./output/app.log", logEntry2);
+//* fs.appendFileSync to write something on the file
+const logEntry1 = `\n ${new Date().toISOString()}`;
+fs.appendFileSync("./output/app.log", logEntry1);
+const logEntry2 = `\n ${new Date().toISOString()}`;
+fs.appendFileSync("./output/app.log", logEntry2);
 
-// console.log("Task complete");
+console.log("Task complete");
 
 console.log("Start reading");
 //* read file with blocking_IO
@@ -20,6 +20,7 @@ const ReadFile2 = fs.readFile("./output/app.log", "utf-8", (error, data) => {
   console.log(data);
 });
 
+console.log(ReadFile);
 console.log(ReadFile2);
 
 console.log(process.platform);
